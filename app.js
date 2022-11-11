8d39c844079856ae7ec1d9ba971b0b6b0b097939
Rachit Chaudhary <37649048+rachit-chaudhary@users.noreply.github.com>
2022-11-11 15:20:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ const express = require("express");
 const app = express();
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/assets");
 });
 app.listen(process.env.PORT || 3000);
