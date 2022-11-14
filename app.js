3c94cce39e7bcfab541b631e24cd8d54da71d865
Rachit Chaudhary <mailracit@gmail.com>
2022-11-14 11:05:50 +0530

    added views engine and public dir

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+const path = require("path");
 const express = require("express");
 const app = express();
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
-});
-app.listen(process.env.PORT || 3000);
+const port = process.env.PORT || 8080
+
+//static file
+app.use(express.static('public'))
+app.use('/model', express.static(__dirname + 'public/model'))
+
+//set views
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+//route for index
+app.get('/', (req, res) => {
+    res.render('index')
+})
+app.listen( port, console.log('server fired up at:' + " " + port ) );
